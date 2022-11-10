b50b734f2b672d4000fbbf551ee18f07f19a2428
JaylenDavey <117655520+JaylenDavey@users.noreply.github.com>
2022-11-10 13:56:53 +0000

    n

--- Assets/Scripts/board.cs
+++ Assets/Scripts/board.cs
@@ -8,9 +8,9 @@ public class board : MonoBehaviour
     public string[] tileNames = new string[] {"Pass Go","Old Kent Road","Community Chest","Income Tax","King's Cross Station","The Angel, Islington","Chance","Euston Road","Pentonville Road","Jail","Pall Mall","Electric Company","Whitehall","Northumberland Avenue","Marylebone Station","Bond Street","Malborough Street","Vine Street","Free Parking","Strand","Chance","Fleet Street","Trafalgar Square","Fenchurch Station","Leicester Square","Coventry Street","Water Works","Piccadilly","Go To Jail","Regent Street","Oxford Street","Community Chest","Bond Street","Liverpool Street Station","Chance","Park Lane","Super Tax","Mayfair"};
 
 
-    public void Start()
+    void Start()
     {
-        Debug.Log(tileNames[1]);
+        Debug.Log(tileNames[1]); 
     }
     
 }
