c4e05664edb5a045122a34c3ed40b9d55fd48c5d
JaylenDavey <117655520+JaylenDavey@users.noreply.github.com>
2022-11-14 15:31:15 +0000

    Fixed stuff

--- Assets/Scripts/board.cs
+++ Assets/Scripts/board.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class board : MonoBehaviour
 {
     public int tileNumber = 0;
-    public string[] tileNames = new string[] {"Pass Go","Old Kent Road","Community Chest","Income Tax","King's Cross Station","The Angel, Islington","Chance","Euston Road","Pentonville Road","Jail","Pall Mall","Electric Company","Whitehall","Northumberland Avenue","Marylebone Station","Bond Street","Malborough Street","Vine Street","Free Parking","Strand","Chance","Fleet Street","Trafalgar Square","Fenchurch Station","Leicester Square","Coventry Street","Water Works","Piccadilly","Go To Jail","Regent Street","Oxford Street","Community Chest","Bond Street","Liverpool Street Station","Chance","Park Lane","Super Tax","Mayfair"};
+    public string[] tileNames = new string[] {"Pass Go","Old Kent Road","Community Chest","Whitechapel Road","Income Tax","King's Cross Station","The Angel, Islington","Chance","Euston Road","Pentonville Road","Jail","Pall Mall","Electric Company","Whitehall","Northumberland Avenue","Marylebone Station","Bond Street","Malborough Street","Vine Street","Free Parking","Strand","Chance","Fleet Street","Trafalgar Square","Fenchurch Station","Leicester Square","Coventry Street","Water Works","Piccadilly","Go To Jail","Regent Street","Oxford Street","Community Chest","Bond Street","Liverpool Street Station","Chance","Park Lane","Super Tax","Mayfair"};
 
 
     void Start()
@@ -14,10 +14,11 @@ public class board : MonoBehaviour
         foreach (string tile in tileNames)
         {
             x++;
-            Debug.Log(tileNames[x]);
+            Debug.Log(tileNames.Length);
+            if(x==39)break;
+
         }
         
-        Debug.Log(tileNames[1]); 
     }
 
 
--- Assets/Scripts/play.cs
+++ Assets/Scripts/play.cs
@@ -1,31 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random;
 
 public class play : MonoBehaviour
 {
     public int tileNumber = 0;
     public string[] tileNames = new string[] {"Pass Go","Old Kent Road","Community Chest","Income Tax","King's Cross Station","The Angel, Islington","Chance","Euston Road","Pentonville Road","Jail","Pall Mall","Electric Company","Whitehall","Northumberland Avenue","Marylebone Station","Bond Street","Malborough Street","Vine Street","Free Parking","Strand","Chance","Fleet Street","Trafalgar Square","Fenchurch Station","Leicester Square","Coventry Street","Water Works","Piccadilly","Go To Jail","Regent Street","Oxford Street","Community Chest","Bond Street","Liverpool Street Station","Chance","Park Lane","Super Tax","Mayfair"};
-
+    public int numberOfDoubles = 0;
 
     void Start()
     {
         Debug.Log(tileNames[1]); 
+        RollDice();
     }
 
-    int RollDice(int diceTotal, bool isDouble)
+    int RollDice()
     {
         int diceRollOne = 0;
         int diceRollTwo = 0;
 
-        diceRollOne = Random.range(0,6);
-        diceRollTwo = Random.range(0,6);
+        diceRollOne = Random.Range(0,6);
+        diceRollTwo = Random.Range(0,6);
 
-        diceTotal = diceRollOne + diceRollTwo;
-        if(diceRollOne == diceRollTwo(isDouble = true));
+        int diceTotal = diceRollOne + diceRollTwo;
 
-        return(diceTotal,isDouble);
+        DoubleCheck(diceRollOne, diceRollTwo);
+
+        Debug.Log(""+diceRollOne+diceRollTwo+DoubleCheck(diceRollOne, diceRollTwo));
+
+        return diceTotal;
+
+    }
+
+    bool DoubleCheck(int a, int b)
+    {
+        if(a==b)
+        {
+            numberOfDoubles ++;
+            return true;
+        }
+        else
+            return false;
     }
     
 }
