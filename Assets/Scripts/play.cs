a0ecadada2891d494a5a5e58f873a090d7c55efa
JaylenDavey <117655520+JaylenDavey@users.noreply.github.com>
2022-11-14 10:02:07 +0000

    reclassed play.cs

--- Assets/Scripts/play.cs
+++ Assets/Scripts/play.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random;
 
-public class board : MonoBehaviour
+public class play : MonoBehaviour
 {
     public int tileNumber = 0;
     public string[] tileNames = new string[] {"Pass Go","Old Kent Road","Community Chest","Income Tax","King's Cross Station","The Angel, Islington","Chance","Euston Road","Pentonville Road","Jail","Pall Mall","Electric Company","Whitehall","Northumberland Avenue","Marylebone Station","Bond Street","Malborough Street","Vine Street","Free Parking","Strand","Chance","Fleet Street","Trafalgar Square","Fenchurch Station","Leicester Square","Coventry Street","Water Works","Piccadilly","Go To Jail","Regent Street","Oxford Street","Community Chest","Bond Street","Liverpool Street Station","Chance","Park Lane","Super Tax","Mayfair"};
