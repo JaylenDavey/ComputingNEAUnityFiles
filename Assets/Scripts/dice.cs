5f5237243c6dea6c28e73079464853d67700096e
JaylenDavey <117655520+JaylenDavey@users.noreply.github.com>
2022-11-14 16:41:43 +0000

    asd

--- Assets/Scripts/dice.cs
+++ Assets/Scripts/dice.cs
@@ -6,18 +6,13 @@ public class dice : MonoBehaviour
 {
     public int numberOfDoubles = 0;
 
-    void Start()
-    {
-        RollDice();
-    }
-
-    int RollDice()
+    public int RollDice()
     {
         int diceRollOne = 0;
         int diceRollTwo = 0;
 
-        diceRollOne = Random.Range(0,6);
-        diceRollTwo = Random.Range(0,6);
+        diceRollOne = Random.Range(1,7);
+        diceRollTwo = Random.Range(1,7);
 
         int diceTotal = diceRollOne + diceRollTwo;
 
@@ -26,18 +21,34 @@ public class dice : MonoBehaviour
         Debug.Log(""+diceRollOne+diceRollTwo+DoubleCheck(diceRollOne, diceRollTwo));
 
         return diceTotal;
-
     }
 
-    bool DoubleCheck(int a, int b)
+    bool DoubleCheck(int dieOne, int dieTwo)
     {
-        if(a==b)
+        if(dieOne==dieTwo)
         {
             numberOfDoubles ++;
+            JailCheck(numberOfDoubles);
             return true;
         }
         else
+            numberOfDoubles = 0;
             return false;
+
+
+    }
+
+    bool JailCheck(int numberOfDoubles)
+    {
+        if(numberOfDoubles == 3)
+        {
+            Debug.LogError($"3 Doubles!!");
+            return true;
+            // player goes to jail
+        }
+        else
+            return false;
+
     }
     
 }
