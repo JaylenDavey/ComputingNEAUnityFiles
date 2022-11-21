1db52057b437047ccbf617eb3d08d64d38ca9f7b
JaylenDavey <117655520+JaylenDavey@users.noreply.github.com>
2022-11-21 19:54:06 +0000

    tileNumber deleted

--- Assets/Scripts/board.cs
+++ Assets/Scripts/board.cs
@@ -7,7 +7,6 @@ public class board : MonoBehaviour
     public dice dice;
     public turns turns;
 
-    public int tileNumber = 0;
     public string[] tileNames = new string[] {"Pass Go","Old Kent Road","Community Chest","Whitechapel Road","Income Tax","King's Cross Station","The Angel, Islington","Chance","Euston Road","Pentonville Road","Jail","Pall Mall","Electric Company","Whitehall","Northumberland Avenue","Marylebone Station","Bow Street","Community Chest","Malborough Street","Vine Street","Free Parking","Strand","Chance","Fleet Street","Trafalgar Square","Fenchurch Station","Leicester Square","Coventry Street","Water Works","Piccadilly","Go To Jail","Regent Street","Oxford Street","Community Chest","Bond Street","Liverpool Street Station","Chance","Park Lane","Super Tax","Mayfair"};
 
     int diceTotal = 0;
