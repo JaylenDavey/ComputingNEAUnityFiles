ad3d5f256fb4e4540be0fc0a74319bcf49417519
JaylenDavey <117655520+JaylenDavey@users.noreply.github.com>
2022-11-22 20:15:37 +0000

    Completed tiles.JSON Added a method for getting all information from tiles.JSON in board.cs All information is stored in an array for each tiles so can be accessed when needed. (potentially need to add a subtype for utillities and stations)

--- Assets/Scripts/board.cs
+++ Assets/Scripts/board.cs
@@ -8,6 +8,38 @@ public class board : MonoBehaviour
     public turns turns;
     public tiles tiles;
 
+    public TextAsset tilesJSON;
+
+    [System.Serializable]
+    public class Tile
+    {
+        public string name;
+        public string type;
+        public int rent;
+        public int price;
+        public int housePrice;
+        public int rentOneHouse;
+        public int rentTwoHouse;
+        public int rentThreeHouse;
+        public int rentFourHouse;
+        public int rentHotel;
+        public bool isOwned;
+        public int owner;
+    }
+
+    [System.Serializable]
+    public class TileList
+    {
+        public Tile[] tile;
+    }
+
+    public TileList tileList = new TileList();
+
+    void Start()
+    {
+        tileList = JsonUtility.FromJson<TileList>(tilesJSON.text);
+    }
+
 
     public int[] playerPositions = new int[] {0,0,0,0,0,0};
     public bool[] playerIsInJail = new bool[] {false,false,false,false,false,false};
